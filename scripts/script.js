de09b5f72451054420a672faf8faf6d25c7dc134
Richard Hogoboom <rhogoboom@gmail.com>
2023-12-03 08:55:35 -0500

    add book to library functionality

--- scripts/script.js
+++ scripts/script.js
@@ -34,22 +34,81 @@ function addBookToLibrary(bookToAdd) {
 testBooks.forEach(book => addBookToLibrary(book));
 
 // Render books
-myLibrary.forEach(book => {
-    const bookCard = document.createElement('div');
-    const cardTitle = document.createElement('h3');
-    const cardContent = document.createElement('ul');
-    const cardAuthor = document.createElement('li');
-    const cardPages = document.createElement('li')
-    const cardRead = document.createElement('li');
+function renderLibrary(library) {
+    const newCards = []
+    library.forEach(book => {
+        const bookCard = document.createElement('div');
+        const cardTitle = document.createElement('h3');
+        const cardContent = document.createElement('ul');
+        const cardAuthor = document.createElement('li');
+        const cardPages = document.createElement('li')
+        const cardRead = document.createElement('li');
+    
+        bookCard.classList.add('book-card');
+    
+        cardTitle.textContent = book.title;
+        cardAuthor.textContent = `By: ${book.author}`;
+        cardPages.textContent = `Pages: ${book.pages}`;
+        cardRead.textContent = `${book.read ? 'Read' : 'Not Read'}`;
+    
+        cardContent.append(cardAuthor, cardPages, cardRead);
+        bookCard.append(cardTitle,cardContent);
 
-    bookCard.classList.add('book-card');
+        newCards.push(bookCard);
+    })
+    contentContainer.replaceChildren(...newCards);
 
-    cardTitle.textContent = book.title;
-    cardAuthor.textContent = `By: ${book.author}`;
-    cardPages.textContent = `Pages: ${book.pages}`;
-    cardRead.textContent = `${book.read ? 'Read' : 'Not Read'}`;
+}
 
-    cardContent.append(cardAuthor, cardPages, cardRead);
-    bookCard.append(cardTitle,cardContent);
-    contentContainer.appendChild(bookCard);
-})
+renderLibrary(myLibrary);
+// myLibrary.forEach(book => {
+//     const bookCard = document.createElement('div');
+//     const cardTitle = document.createElement('h3');
+//     const cardContent = document.createElement('ul');
+//     const cardAuthor = document.createElement('li');
+//     const cardPages = document.createElement('li')
+//     const cardRead = document.createElement('li');
+
+//     bookCard.classList.add('book-card');
+
+//     cardTitle.textContent = book.title;
+//     cardAuthor.textContent = `By: ${book.author}`;
+//     cardPages.textContent = `Pages: ${book.pages}`;
+//     cardRead.textContent = `${book.read ? 'Read' : 'Not Read'}`;
+
+//     cardContent.append(cardAuthor, cardPages, cardRead);
+//     bookCard.append(cardTitle,cardContent);
+//     contentContainer.appendChild(bookCard);
+// })
+
+const dialog = document.querySelector('dialog');
+const showButton = document.querySelector('dialog + button');
+const submitButton = document.querySelector('#add-new-book')
+const closeButton = document.querySelector('#cancel-add-new-book');
+
+showButton.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+closeButton.addEventListener('click', () => {
+    dialog.close();
+});
+
+submitButton.addEventListener('click', (e) => {
+    const addForm = document.querySelector('#add-book-form')
+    const formInputs = [...document.querySelectorAll('#add-book-form input')];
+    if(!formInputs.every(input => input.validity.valid)) {
+        return
+    }
+    const bookTitle = document.querySelector('#book-title').value;
+        const bookAuthor = document.querySelector('#author-name').value;
+        const bookPages = parseInt(document.querySelector('#pages').value);
+        const bookRead = document.querySelector('#read').checked;
+
+        addBookToLibrary(new Book(bookTitle, bookAuthor, bookPages, bookRead));
+        renderLibrary(myLibrary);
+        e.preventDefault();
+        addForm.reset()
+
+    dialog.close();
+} )
